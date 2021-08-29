e2e1f62c73787a556aed0a2e43179db0c0a3211f
Vishal Sharma <vishalsharma@Vishals-MacBook-Air.local>
2021-08-29 01:04:11 -0700

    integrated eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,25 @@
-import {
-  Box, ChakraProvider, theme, VStack
-} from "@chakra-ui/react"
-import * as React from "react"
-import CentreDiv from "../components/CentreDiv/CentreDiv"
-import Contact from "../components/Contact/Contact"
-import Introduction from "../components/Introduction/Introduction"
-import NavBar from "../components/Navbar/Navbar"
-import Projects from "../components/Projects/Projects"
-import Skills from "../components/Skills/Skills"
+import { Box, ChakraProvider, theme, VStack } from '@chakra-ui/react';
+import * as React from 'react';
+import CentreDiv from '../components/CentreDiv/CentreDiv';
+import Contact from '../components/Contact/Contact';
+import Introduction from '../components/Introduction/Introduction';
+import NavBar from '../components/Navbar/Navbar';
+import Projects from '../components/Projects/Projects';
+import Skills from '../components/Skills/Skills';
 
-export const App = () => (
+export const App = (): React.ReactElement => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-    <NavBar />
+      <NavBar />
       {/* <Grid p={3}> */}
-        <VStack spacing={8}>
-          <Introduction />
-          <CentreDiv />
-          <Projects />
-          <Skills />
-          <Contact />
-        </VStack>
+      <VStack spacing={8}>
+        <Introduction />
+        <CentreDiv />
+        <Projects />
+        <Skills />
+        <Contact />
+      </VStack>
       {/* </Grid> */}
     </Box>
   </ChakraProvider>
-)
+);
--- src/components/Skills/Skills.ts
+++ src/components/Skills/Skills.ts
@@ -1,1 +1,16 @@
-export const SKILLS:Array<string> = ['ReactJS' ,'Angular','HTML/CSS','Python','TypeScript','JavaScript','SQL','React Native','Git','FireBase','IBM Bluemix','Docker','MySQL','Agile Workflows'];
+export const SKILLS: Array<string> = [
+  'ReactJS',
+  'Angular',
+  'HTML/CSS',
+  'Python',
+  'TypeScript',
+  'JavaScript',
+  'SQL',
+  'React Native',
+  'Git',
+  'FireBase',
+  'IBM Bluemix',
+  'Docker',
+  'MySQL',
+  'Agile Workflows',
+];
